cdcdefea4efe9e1c7e1872dbdc5f5469e1fff020
conniepope <conniepope77@gmail.com>
2019-08-22 12:08:26 -0400

    Article model set up

--- models/Article.js
+++ models/Article.js
@@ -1,2 +1,27 @@
 // Variable for mongoose Schema contructor
 var Schema = mongoose.Schema;
+
+//Using the Schema constructor, create a new Schema object
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+})
+
+// Create model from Schema using mongoose method
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
